7f99e3112a2cae10f22dd67ee1626cf91b0875c6
heyuanpeng <991800152@qq.com>
2021-05-18 14:08:36 +0800

    node：创建一个完整的post、get请求进行服务器请求

--- http.js
+++ http.js
@@ -4,6 +4,14 @@ var url = require("url");
 // 2. 引入 http 模块
 var http = require("http");
 
+// 虚拟 SQL 读取出来的数据
+var items = [
+  {
+    name: "hyp",
+    age: 18,
+  },
+];
+
 // 3. 用 http 模块创建服务
 /**
  * req 获取 url 信息 (request)
@@ -11,6 +19,12 @@ var http = require("http");
  */
 http
   .createServer(function (req, res) {
+    // 设置跨域的域名，* 代表允许任意域名跨域
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // 设置 header 类型
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    // 跨域允许的请求方式
+    res.setHeader("Content-Type", "application/json");
     // 4. 获取服务器请求
     /**
      * 访问地址是：http://localhost:3000/?userName=hyp
@@ -21,11 +35,6 @@ http
      */
     if (req.url != "/favicon.ico") {
       // 5. 使用 url 的 parse 方法
-      /**
-       * parse 方法需要两个参数：
-       * 第一个参数是地址
-       * 第二个参数是 true 的话表示把 get 传值转换成对象
-       */
       var result = url.parse(req.url, true);
       console.log(result);
       console.log(result.query.userName);
@@ -46,17 +55,40 @@ http
       console.log(Format);
       const Resolve = url.resolve(req.url, "heYP");
       console.log(Resolve);
+      // 判断请求
+      switch (req.method) {
+        // post 请求时，浏览器会先发一次 options 请求，如果请求通过，则继续发送正式的 post 请求
+        case "OPTIONS":
+          res.statusCode = 200;
+          res.end();
+          break;
+
+        // 如果是 get 请求，则直接返回 items 数组
+        case "GET":
+          let data = JSON.stringify(items);
+          res.write(data);
+          res.end();
+          break;
+
+        // 如果是 post 请求
+        case "POST":
+          let item = "";
+          // 读取每次发送的数据
+          req.on("data", function (chunk) {
+            item += chunk;
+          });
+          // 数据发送完成
+          req.on("end", function () {
+            // 存入
+            item = JSON.parse(item);
+            items.push(item.item);
+            // 将数据返回到客户端
+            let data = JSON.stringify(items);
+            res.write(data);
+            res.end();
+          });
+          break;
+      }
     }
-
-    // 设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf8
-    res.writeHead(200, {
-      "Content-Type": "text/html;charset=UTF-8",
-    });
-
-    // 往页面打印值
-    res.write('<h1 style="text-align:center">Hello</h1>');
-
-    // 结束响应
-    res.end();
   })
   .listen(3000);
