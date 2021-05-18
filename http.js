a4f4434948d321be7834edf3ae7d6ca08cbda9a0
heyuanpeng <991800152@qq.com>
2021-05-18 10:47:10 +0800

    node实例：获取地址上面的参数

---
+++ http.js
@@ -0,0 +1,62 @@
+// 1. 引入 url 模块
+var url = require("url");
+
+// 2. 引入 http 模块
+var http = require("http");
+
+// 3. 用 http 模块创建服务
+/**
+ * req 获取 url 信息 (request)
+ * res 浏览器返回响应信息 (response)
+ */
+http
+  .createServer(function (req, res) {
+    // 4. 获取服务器请求
+    /**
+     * 访问地址是：http://localhost:3000/?userName=hyp
+     * 如果你执行 console.log(req.url)，它将执行两次，分别返回下面的信息：
+     * /  ?userName=hyp
+     * /  /favicon.ico
+     * 这里为了防止重复执行，所以排除 req.url == /favicon.ico 的情况
+     */
+    if (req.url != "/favicon.ico") {
+      // 5. 使用 url 的 parse 方法
+      /**
+       * parse 方法需要两个参数：
+       * 第一个参数是地址
+       * 第二个参数是 true 的话表示把 get 传值转换成对象
+       */
+      var result = url.parse(req.url, true);
+      console.log(result);
+      console.log(result.query.userName);
+      const Format = url.format({
+        protocol: null,
+        slashes: null,
+        auth: null,
+        host: null,
+        port: null,
+        hostname: null,
+        hash: null,
+        search: "?userName=hyp",
+        query: { userName: "hyp" },
+        pathname: "/",
+        path: "/?userName=hyp",
+        href: "/?userName=hyp",
+      });
+      console.log(Format);
+      const Resolve = url.resolve(req.url, "heYP");
+      console.log(Resolve);
+    }
+
+    // 设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf8
+    res.writeHead(200, {
+      "Content-Type": "text/html;charset=UTF-8",
+    });
+
+    // 往页面打印值
+    res.write('<h1 style="text-align:center">Hello</h1>');
+
+    // 结束响应
+    res.end();
+  })
+  .listen(3000);
