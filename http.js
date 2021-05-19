7efda806553e1b3646c29b5abd9ba30571f17551
heyuanpeng <991800152@qq.com>
2021-05-19 17:26:23 +0800

    实现mySQL数据库连接，并实现读取数据和新增数据

--- http.js
+++ http.js
@@ -1,16 +1,21 @@
 // 1. 引入 url 模块
-var url = require("url");
+const url = require("url");
 
 // 2. 引入 http 模块
-var http = require("http");
+const http = require("http");
 
-// 虚拟 SQL 读取出来的数据
-var items = [
-  {
-    name: "hyp",
-    age: 18,
-  },
-];
+// 3、数据库连接
+
+const mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "hyp",
+});
+// SQL新增功能
+let addSqlParams = ["1", "2", "3"],
+  items;
 
 // 3. 用 http 模块创建服务
 /**
@@ -34,27 +39,6 @@ http
      * 这里为了防止重复执行，所以排除 req.url == /favicon.ico 的情况
      */
     if (req.url != "/favicon.ico") {
-      // 5. 使用 url 的 parse 方法
-      var result = url.parse(req.url, true);
-      console.log(result);
-      console.log(result.query.userName);
-      const Format = url.format({
-        protocol: null,
-        slashes: null,
-        auth: null,
-        host: null,
-        port: null,
-        hostname: null,
-        hash: null,
-        search: "?userName=hyp",
-        query: { userName: "hyp" },
-        pathname: "/",
-        path: "/?userName=hyp",
-        href: "/?userName=hyp",
-      });
-      console.log(Format);
-      const Resolve = url.resolve(req.url, "heYP");
-      console.log(Resolve);
       // 判断请求
       switch (req.method) {
         // post 请求时，浏览器会先发一次 options 请求，如果请求通过，则继续发送正式的 post 请求
@@ -63,8 +47,9 @@ http
           res.end();
           break;
 
-        // 如果是 get 请求，则直接返回 items 数组
+        // 如果是 get 请求，
         case "GET":
+          // 查询数据库数据
           let data = JSON.stringify(items);
           res.write(data);
           res.end();
@@ -80,8 +65,11 @@ http
           // 数据发送完成
           req.on("end", function () {
             // 存入
-            item = JSON.parse(item);
-            items.push(item.item);
+            addSqlParams = Object.values(JSON.parse(item));
+            // 新增的 SQL 语句及新增的字段信息
+            let addSql = "INSERT INTO hypData(name,age,gender) VALUES(?,?,?)";
+            SqlFn(addSql, addSqlParams);
+            console.log(item);
             // 将数据返回到客户端
             let data = JSON.stringify(items);
             res.write(data);
@@ -92,3 +80,19 @@ http
     }
   })
   .listen(3000);
+
+connection.query("SELECT * FROM hypData", function (error, results) {
+  if (error) throw error;
+  items = results;
+});
+const SqlFn = function (addSql, addSqlParams) {
+  // 连接 SQL 并实施语句
+  connection.query(addSql, addSqlParams, function (err, res) {
+    if (err) {
+      console.log("新增错误：", err);
+      return;
+    } else {
+      console.log("新增成功：", res);
+    }
+  });
+};
